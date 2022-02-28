a0cf64129b2c43e16b318422769753ca3ccf9401
akkayaberkin <berkin654@gmail.com>
2022-02-28 13:30:10 +0300

    Add project files.

---
+++ MyEvernote.WebApp/Scripts/navBar.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+
+    $(".nav-item").click(function () {
+        var btn = $(this);
+        var liked = btn.data("liked");
+        var noteid = btn.data("note-id");
+        var spanStar = btn.find("span.like-star");
+        var spanCount = btn.find("span.like-count");
+
+
+
+    });
+}
